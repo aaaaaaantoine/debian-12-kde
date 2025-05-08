b3f4fe9e57b35c6aea2acfa23cff7eae64323ec0
Antoine DX <antoine.ducroux3@gmail.com>
2025-05-08 18:52:09 +0200

    Update debian-workstation.sh

--- debian-workstation.sh
+++ debian-workstation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Script post-installation d'une Debian 12 Bookworm avec l'environnement KDE Plasma
+# Script à exécuter après l'installation de Debian depuis l'image ISO Netinstall avec le bureau KDE Plasma
+# Commentez les sections qui ne vous intéressent pas.
+# Pour l'utiliser vous aurez besoin de sudo et que votre utilisateur soit dans ce groupe.
 
-echo "-------------------------------"
-echo "Ce script"
-echo "-------------------------------"
+echo "--------------------------------"
+echo "Script de Debian à usage général"
+echo "--------------------------------"
 
 # Ajout du dépôt Backports
 echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware" | tee -a /etc/apt/sources.list
@@ -19,7 +21,41 @@ sudo apt install -y \
 akregator \
 ark \
 calligra \
-calligra-gemini calligraplan calligrasheets calligrastage calligrawords dolphin dragonplayer elisa gwenview kaccounts-integration kaccounts-providers kaddressbook karbon kcalc kdepim-addons kdenlive kdeconnect kde-spectacle kid3 kmail konqueror konsole kontact konversation korganizer krita ksystemlog ktorrent kwalletmanager kwave kwrite okular partitionmanager plasma-bigscreen plasma-discover-backend-flatpak plasma-firewall plasma-vault plasma-wallpapers-addons plasma-workspace-wallpapers print-manager yakuake -y
+calligra-gemini \
+calligraplan \
+calligrasheets \
+calligrastage \
+calligrawords \
+dolphin \
+dragonplayer \
+elisa \
+gwenview \
+kaddressbook \
+karbon \
+kcalc \
+kdepim-addons \
+kdenlive \
+kdeconnect \
+kde-spectacle \
+kid3 \
+kmail \
+konqueror \
+konsole \
+kontact \
+konversation \
+korganizer \
+krita \
+ksystemlog \
+ktorrent \
+kwalletmanager \
+kwave \
+kwrite \
+okular \
+partitionmanager \
+plasma-discover-backend-flatpak \
+plasma-firewall \
+plasma-vault \
+yakuake
 
 # Installation de Kodi Media Center
 sudo apt install kodi -y
@@ -37,3 +73,5 @@ sudo apt install virtualbox -y
 sudo gpasswd -a antoine vboxusers
 #sudo apt install virtualbox-ext-pack -y
 #sudo apt install virtualbox-guest-utils -y
+
+echo "Le script post-installation est terminé, veuillez redémarrer le système"
