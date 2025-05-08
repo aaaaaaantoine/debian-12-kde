8100cd60cea91e4d9ca7bf3c4faa2298e661a4d2
Antoine DX <antoine.ducroux3@gmail.com>
2025-05-08 19:37:38 +0200

    Update debian-smb.sh

--- debian-smb.sh
+++ debian-smb.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-apt update && apt full-upgrade -y
-apt install -y samba
+sudo apt update && apt full-upgrade -y
+sudo apt install -y samba
 
-cp /etc/samba/smb.conf /etc/samba/smb.conf.old
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.old
 
-echo "[Vidéos]
+sudo echo "[Vidéos]
    comment = Partage de Vidéos
    path = /home/antoine/Vidéos
    guest ok = yes
    read only = yes
    browseable = yes" | tee -a /etc/samba/smb.conf
 
-echo "[Musique]
+sudo echo "[Musique]
    comment = Partage de Musique
    path = /home/antoine/Musique
    guest ok = yes
@@ -20,6 +20,6 @@ echo "[Musique]
    browseable = yes" | tee -a /etc/samba/smb.conf
 
 echo "Donnez un mot de passe Samba pour l'utilisateur Antoine"
-smbpasswd -a antoine
+sudo smbpasswd -a antoine
   
-systemctl enable --now nmbd smbd
+sudo systemctl enable --now nmbd smbd
