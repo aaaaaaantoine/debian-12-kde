6c223b332a9bf6a54f34a45d5501685ce5d63fd2
Antoine DX <antoine.ducroux3@gmail.com>
2025-05-09 15:35:05 +0200

    Update debian-kde.sh

--- debian-kde.sh
+++ debian-kde.sh
@@ -8,9 +8,26 @@ echo "--------------------------------"
 echo "Script de Debian à usage général"
 echo "--------------------------------"
 
+# Utilitaires que je supprime
+sudo apt autoremove -y \
+gimp* \
+juk \
+kmag \
+kmousetool \
+kmouth \
+konqueror \
+libreoffice* \
+sweeper \
+xterm
+
 # Ajout du dépôt Backports
 echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware" | tee -a /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware" | tee -a /etc/apt/sources.list
+
+# Ajout du dépôt VirtualBox
+sudo apt install fasttrack-archive-keyring -y
+sudo echo "deb http://fasttrack.debian.net/debian-fasttrack/ bookworm-fasttrack main contrib" | sudo tee /etc/apt/sources.list.d/fasttrack.list
+sudo echo "deb http://fasttrack.debian.net/debian-fasttrack/ bookworm-backports-staging main contrib" | sudo tee -a /etc/apt/sources.list.d/fasttrack.list
 
 # Mise à jour du système 
 sudo apt update
@@ -18,65 +35,39 @@ sudo apt full-upgrade -y
 
 # Installation mes utilitaires pour KDE Plasma 
 sudo apt install -y \
-akregator \
-ark \
 calligra \
 calligra-gemini \
 calligraplan \
 calligrasheets \
 calligrastage \
 calligrawords \
-dolphin \
-dragonplayer \
 elisa \
-falkon \
-gwenview \
-kaddressbook \
-karbon \
-kcalc \
 kdepim-addons \
 kdenlive \
-kdeconnect \
-kde-spectacle \
 kid3 \
-kmail \
-konsole \
 kontact \
 konversation \
-korganizer \
 krita \
 ksystemlog \
 ktorrent \
 kwalletmanager \
 kwave \
-kwrite \
-okular \
-partitionmanager \
 plasma-firewall \
-plasma-vault \
 yakuake
 
 # Installation de Kodi Media Center
 sudo apt install kodi -y
 
 # Utilitaires en ligne de commande
-sudo apt install -y\
-bash-completion \
+sudo apt install -y \
 ufw \
-vim \
-wget
+vim
 
 # Installation du script Youtube-dl depuis le dépôt Backpors
 sudo apt install yt-dlp -t bookworm-backports -y
 
 # Installation de VirtualBox
-sudo apt install fasttrack-archive-keyring -y
-sudo echo "deb http://fasttrack.debian.net/debian-fasttrack/ bookworm-fasttrack main contrib" | sudo tee /etc/apt/sources.list.d/fasttrack.list
-sudo echo "deb http://fasttrack.debian.net/debian-fasttrack/ bookworm-backports-staging main contrib" | sudo tee -a /etc/apt/sources.list.d/fasttrack.list
-sudo apt update
-sudo apt install virtualbox -y
+sudo apt install virtualbox virtualbox-ext-pack -y
 sudo gpasswd -a antoine vboxusers
-#sudo apt install virtualbox-ext-pack -y
-#sudo apt install virtualbox-guest-utils -y
 
 echo "Le script post-installation est terminé, veuillez redémarrer le système"
