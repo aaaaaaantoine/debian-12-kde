81975aca4248a98810f4a682b5122d2a6880e967
Antoine DX <antoine.ducroux3@gmail.com>
2025-06-03 10:39:20 +0200

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script à exécuter après l'installation de Debian depuis l'image ISO Netinstall.
+# Commentez les sections qui ne vous intéressent pas.
+# Pour l'utiliser vous aurez besoin de sudo et que votre utilisateur soit dans ce groupe.
+
+echo "--------------------------------"
+echo "Script de Debian à usage général"
+echo "--------------------------------"
+
+# Utilitaires que je supprime
