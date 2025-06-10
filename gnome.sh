943f562800e8ba272ea3088868f2c30f60ace44a
Antoine DX <antoine.ducroux3@gmail.com>
2025-06-10 21:37:04 +0200

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script à exécuter après l'installation de Debian depuis l'image ISO Netinstall.
+# Script à exécuter après l'installation de Debian depuis l'image ISO Minimal Netinstall
 # Commentez les sections qui ne vous intéressent pas.
 # Pour l'utiliser vous aurez besoin de sudo et que votre utilisateur soit dans ce groupe.
 
@@ -8,4 +8,55 @@ echo "--------------------------------"
 echo "Script de Debian à usage général"
 echo "--------------------------------"
 
+# Mise à jour du système 
+sudo apt update
+sudo apt full-upgrade -y
+
+# Installation de Flatpak
+sudo apt install -y flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Installation mes utilitaires pour GNOME
+sudo apt install -y \
+abiword \
+alacarte \
+celluloid \
+curl \
+deja-dup \
+epiphany-browser \
+geary \
+gnome-builder \
+gnome-calendar \
+gnome-console \
+gnome-core \
+gnome-music \
+gnome-software-plugin-flatpak \
+gnucash \
+gnumeric \
+git \
+kodi \
+secrets \
+shortwave \
+ufw \
+vim \
+virt-manager
+
+# Installation mes utilitaires Flatpak pour GNOME
+sudo flatpak install -y flathub app.drey.EarTag
+sudo flatpak install -y flathub io.gitlab.news_flash.NewsFlash
+sudo flatpak install -y flathub org.gnome.Podcasts
+
+# Installation du script Youtube-dl
+sudo apt install -y yt-dlp
+
 # Utilitaires que je supprime
+sudo apt autoremove -y \
+evince \
+gnome-characters \
+gnome-font-viewer \
+gnome-logs \
+gnome-snapshot \
+simple-scan \
+totem
+
+echo "Le script post-installation est terminé, veuillez redémarrer le système"
